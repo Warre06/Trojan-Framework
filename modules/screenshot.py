5db280a611716d4265a65c1332800a326596a345
Warre06 <warregehre06@gmail.com>
2023-06-01 21:34:02 +0200

    Update screenshot.py

--- modules/screenshot.py
+++ modules/screenshot.py
@@ -3,14 +3,8 @@ from datetime import datetime
 
 class ScreenshotMaker:
     def __init__(self):
-        self.screenshot_folder = "screenshots/"
+        self.name = datetime.now().strftime("%d%m%y-%H%M%S")
     
-    def start(self):
-        screenshot = pyautogui.screenshot()
-        current_time = datetime.now().strftime("%d%m%y-%H%M%S")
-        screenshot_path = f'screenshots/screenshot_{current_time}.png'
-        screenshot.save(screenshot_path)
-
-
-screenshot = ScreenshotMaker()
-screenshot.start()
+    def main(self):
+        screenshot = pyautogui.screenshot(self.name)
+        return screenshot
